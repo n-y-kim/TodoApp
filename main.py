7455bd89472ba43d41fce89d61291507e093c966
Boram Jeong <annajeong@microsoft.com>
2023-03-12 21:09:10 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=FileResponse)
+def redirect_index():
+    return "static/index.html"
+
+@app.get("/hcheck", status_code=200)
+def health_check():
+    return "OK"
